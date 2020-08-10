0e21a6126207d6ce6f26f26d3cccc8b7841b015d
Donray Williams <w.donray@yahoo.com>
2020-08-10 12:54:02 -0500

    Updated GetMatchHistory

--- services/getMatchHistory/handler.ts
+++ services/getMatchHistory/handler.ts
@@ -1,13 +1,16 @@
 import axios, { AxiosResponse } from "axios";
 import { failure, success } from "../../utils/response";
 import { getRiotAPIKey } from "../../utils/secrets";
+import { AnyMxRecord } from "dns";
 
 export const main = async (event: any = {}, _context) => {
   try {
-    const { region, encryptedAccountId, count } = JSON.parse(event.body);
+    const { region, encryptedAccountId, endIndex } = JSON.parse(event.body);
 
-    if (!region || !encryptedAccountId) {
-      return failure({ error: "Must provide Encrypted Account Id and Region" });
+    if (!region || !encryptedAccountId || !endIndex) {
+      return failure({
+        error: "Must provide Encrypted Account Id, Region and End Index",
+      });
     }
 
     const apiKey = await getRiotAPIKey();
@@ -19,12 +22,38 @@ export const main = async (event: any = {}, _context) => {
     const response: AxiosResponse = await axios.get(url, {
       params: {
         api_key: apiKey,
-        endIndex: count ? count : 10,
+        endIndex: endIndex,
       },
     });
+    const matches: Array<AnyMxRecord> = [];
 
     if (response.status === 200) {
-      return success(response.data);
+      const urls: Array<any> = [];
+      response.data.matches.forEach((match: any) => {
+        urls.push(
+          axios.get(`https://${region}/lol/match/v4/matches/${match.gameId}`, {
+            params: {
+              api_key: apiKey,
+            },
+          })
+        );
+      });
+      await axios
+        .all(urls)
+        .then(
+          axios.spread((...responses) => {
+            responses.forEach((matchData: any) => {
+              matches.push(matchData.data);
+            });
+          })
+        )
+        .catch((err) => {
+          return failure({
+            error: "Riot API Failed",
+            response: err,
+          });
+        });
+      return success(matches);
     } else {
       return failure({
         error: "Riot API Failed",
